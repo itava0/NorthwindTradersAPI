da2588a77e3042cc902b5f619dec1fc52ad77ffc
Ivan Tavarez <ivantavarez2@gmail.com>
2023-12-21 11:56:14 -0500

    Created controlled

--- src/main/java/org/pluralsight/controllers/CategoryController.java
+++ src/main/java/org/pluralsight/controllers/CategoryController.java
@@ -1,6 +1,8 @@
 package org.pluralsight.controllers;
 
+import org.pluralsight.dao.CategoryDao;
 import org.pluralsight.models.Category;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,32 +16,23 @@ public class CategoryController {
     //http://localhost:8080/categories
     //http://localhost:8080/categories/17
 
+    private final CategoryDao categoryDao;
+
+    @Autowired
+    public CategoryController(CategoryDao categoryDao) {
+        this.categoryDao = categoryDao;
+    }
+
+
     @RequestMapping(path="/categories", method= RequestMethod.GET)
     public List<Category> getCategories(){
-        List<Category> categories = new ArrayList<>();
-        categories.add(new Category(1, "Electronics"));
-        categories.add(new Category(2, "Electronics"));
-        categories.add(new Category(3,  "Appliances"));
-        categories.add(new Category(4,  "Fitness"));
-        categories.add(new Category(5,  "Electronics"));
-        categories.add(new Category(6,  "Electronics"));
-        categories.add(new Category(7,  "Computers"));
-        categories.add(new Category(8,  "Computers"));
-        categories.add(new Category(9,  "Computers"));
+        List<Category> categories = categoryDao.getAll();
 
         return categories;
     }
 
     @RequestMapping(path="/categories/{id}", method=RequestMethod.GET)
-    public Category getProductById(@PathVariable int id){
-        List<Category> categories = getCategories();
-
-        // Find the product with the specified ID
-        for (Category category : categories) {
-            if (category.getCategoryId() == id) {
-                return category;
-            }
-        }
-        return null;
+    public Category getProductById(@PathVariable int id) {
+        return categoryDao.getCategoryBYId(id);
     }
 }
